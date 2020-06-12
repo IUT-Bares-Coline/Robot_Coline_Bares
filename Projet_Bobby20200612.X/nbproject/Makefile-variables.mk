4900c0c023b7985a60dc82fe9bc2a8595437a8eb
chocovore <coline.bares@gmail.com>
2020-06-12 15:59:33 +0200

    modif debut partie 3

---
+++ Projet_Bobby20200612.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Projet_Bobby20200312.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Projet_Bobby20200312.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=projetbobby20200312.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/projetbobby20200312.x.tar
