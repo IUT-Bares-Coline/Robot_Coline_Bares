4ff0de74bed626665773a2d08e01684963096b78
TABLE2 <TABLE2>
2020-09-07 10:57:01 +0200

    07.09

--- C#/projetrobotCB/projetrobotCB/MainWindow.xaml.cs
+++ C#/projetrobotCB/projetrobotCB/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExtendedSerialPort;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,15 +21,24 @@ namespace projetrobotCB
     /// </summary>
     public partial class MainWindow : Window
     {
+        ReliableSerialPort serialPort1;
         public MainWindow()
         {
             InitializeComponent();
+            serialPort1 = new ReliableSerialPort("COM8", 115200, System.IO.Ports.Parity.None, 8, System.IO.Ports.StopBits.One);
+            serialPort1.DataReceived += SerialPort1_DataReceived;
+            serialPort1.Open();
+        }
+        
+        public void SerialPort1_DataReceived(object sender, DataReceivedArgs e)
+        {
+            textBoxReception.Text += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length) ;
         }
 
         private void SendMessage()
         {
-        //textBoxEmission.Text = "bob";
-        textBoxReception.Text = "\nReçu : " + textBoxEmission.Text;
+        serialPort1.WriteLine(textBoxEmission.Text); 
+        //textBoxReception.Text = textBoxReception.Text + "\nReçu : " + textBoxEmission.Text;
         textBoxEmission.Text = "";
         }
 
--- test.c
+++
@@ -1,9 +0,0 @@
-namespace Robot
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
-}
